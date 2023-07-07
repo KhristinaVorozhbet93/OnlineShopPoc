861c714f9d215246745bd715621591b427bb8ba2
Voro_to54 <Voro_to54@itstepru.onmicrosoft.com>
2023-07-07 21:09:52 +0400

    Реализована задержка перед каждым ретраем с помощью декоратора

--- OnlineShopPoc/AppStartedNotificatorBackgroundServicer.cs
+++ OnlineShopPoc/AppStartedNotificatorBackgroundServicer.cs
@@ -1,9 +1,12 @@
-﻿namespace OnlineShopPoc
+﻿using System.Diagnostics;
+
+namespace OnlineShopPoc
 {
     public class AppStartedNotificatorBackgroundServer : BackgroundService
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger<AppStartedNotificatorBackgroundServer> _logger;
+
         public AppStartedNotificatorBackgroundServer(IServiceProvider serviceProvider,
             ILogger<AppStartedNotificatorBackgroundServer> logger)
         {
@@ -13,36 +16,18 @@
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            int attemptLimit = 2;
-            bool isSend = false;
-
             while (!stoppingToken.IsCancellationRequested)
             {
                 await using (var scope = _serviceProvider.CreateAsyncScope())
                 {
+                    var email = "ptykhina.khristi@mail.ru"; 
                     var emailSender = scope.ServiceProvider.GetRequiredService<IEmailSender>();
-                    for (int i = 0; i < attemptLimit && isSend == false; i++)
-                    {
-                        try
-                        {
-                            await emailSender.SendEmailAsync
-                                ("ptykhina.khristi@mail.ru", "Приложение запущено", "Приложение запущено!");
-                            isSend = true;
-                        }
-                        catch (Exception e)
-                        {
-                            if (i == 0)
-                            {
-                                _logger.LogWarning(e, "Сообщение не отправлено!");
-                            }
-                            else
-                            {
-                                _logger.LogError(e, "Сообщение не отправлено повторно!");
-                            }
-                        }
-                    }
-                    await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+
+                    await emailSender.SendEmailAsync(email, "Промоакции", "Список акций");
+                    _logger.LogInformation("Email sent to {email} in {ElapsedMilliseconds} ms",
+                        email);
                 }
+                await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
             }
         }
     }
---
+++ OnlineShopPoc/EmailSenderRetryDecorator.cs
@@ -0,0 +1,49 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace OnlineShopPoc
+{
+    public class EmailSenderRetryDecorator
+    {
+        private readonly IEmailSender _emailSenderImplementation;
+        private readonly ILogger<EmailSenderRetryDecorator> _logger;
+        private readonly int _attemptLimit;
+        private readonly AsyncRetryPolicy? _policy;
+        CancellationTokenSource tokenSource;
+
+        public EmailSenderRetryDecorator(IEmailSender emailSenderImplementation,
+            ILogger<EmailSenderRetryDecorator> logger,
+            IConfiguration configuration)
+        {
+            _emailSenderImplementation = emailSenderImplementation 
+                ?? throw new ArgumentNullException(nameof(emailSenderImplementation));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            if (configuration is null) throw new ArgumentNullException(nameof(configuration));
+            _attemptLimit = configuration.GetValue<int>("SalesEmailAttemptCount"); 
+            tokenSource = new CancellationTokenSource();
+
+            _policy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(retryCount: _attemptLimit,
+                                    sleepDurationProvider: (retryCount) => TimeSpan.FromSeconds(retryCount * 2),
+                                    onRetry: (exception, sleepDuration, attemptNumber) =>
+                                    {
+                                        _logger.LogWarning(exception,
+                                    "Ошибка! Сообщение отправить повторно через {SleepDuration}.Попытка номер: {RetryCount}.", sleepDuration, attemptNumber, );
+                                    });
+
+        }
+
+        public async Task SendEmailAsync(string recepientEmail, string subject, string message)
+        {
+            var sendResult = await _policy!.ExecuteAndCaptureAsync(
+                _ => _emailSenderImplementation.SendEmailAsync(recepientEmail, subject, message),tokenSource.Token);
+
+            if (sendResult.Outcome == OutcomeType.Failure)
+            {
+                _logger.LogError(sendResult.FinalException, "Сообщение не отправлено!Ошибка!");
+            }
+        }
+    }
+}
+
